9b8256994428a739e191a6ba6b67fe501d3a966c
Leo <xinming.lai@qq.com>
2018-12-09 22:39:49 +0800

    跑一下word2vec

--- trajectory2nodes.py
+++ trajectory2nodes.py
@@ -1,4 +1,5 @@
 import psycopg2
+from gensim.models import Word2Vec
 
 # 数据库连接参数
 conn = psycopg2.connect(database="porto", user="osmuser", password="pass", host="localhost", port="5432")
@@ -9,13 +10,16 @@ rows = cur.fetchall()        # all rows in table
 
 way2nodes = {}  # {osmid_way: [osmid_node,...]}
 for row in rows:
-    way2nodes[row[0]] = row[1]
+    osmid_way = row[0]
+    way2nodes[str(osmid_way)] = row[1]
 conn.commit()
 cur.close()
 conn.close()
 
+
+walks = []
 f_500K_ways = open(r'500k.w2v.txt', 'r')
-f_500K_nodes = open(r'500k.nodes.txt', 'w+')
+f_500K_node2vec = open(r'500k.nodes.vector', 'w+')
 for line in f_500K_ways.readlines():
     output_nodes = []
     way_list = line.strip().split(" ")
@@ -24,17 +28,22 @@ for line in f_500K_ways.readlines():
         way = way.strip()
         if len(way) < 1 : continue
         osmid_way, heading = way.split("-")
-        if int(osmid_way) not in way2nodes: continue
-        nodes = way2nodes[int(osmid_way)]
+        if osmid_way not in way2nodes: continue
+        nodes = way2nodes[osmid_way]
         if heading == "forward":
             for node in nodes:
-                output_nodes.append(node)
+                output_nodes.append(str(node))
         else:
             index = len(nodes) - 1
             while index >= 0:
                 node = nodes[index]
                 output_nodes.append(node)
-    f_500K_nodes.write(str(output_nodes) + '\n')
+                index -= 1
+    # f_500K_nodes.write(str(output_nodes) + '\n')
+    walks.append(output_nodes)
+
+model = Word2Vec(walks, size=64, window=5, min_count=0, sg=1, hs=1, workers=4)
+model.wv.save_word2vec_format(f_500K_node2vec)
 
 f_500K_ways.close()
-f_500K_nodes.close()
+f_500K_node2vec.close()
