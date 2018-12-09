5e02671c86c6a39d155c9c9ac322668ba716ec8b
Leo Bright <buxingzhexiong@sina.com>
2018-12-10 00:08:20 +0800

    跑word2vec

--- trajectory2nodes.py
+++ trajectory2nodes.py
@@ -3,25 +3,25 @@ from gensim.models import Word2Vec
 import json
 
 # 数据库连接参数
-conn = psycopg2.connect(database="porto", user="osmuser", password="pass", host="localhost", port="5432")
-cur = conn.cursor()
-
-cur.execute("select id, nodes from ways;")
-rows = cur.fetchall()        # all rows in table
-
-way2nodes = {}  # {osmid_way: [osmid_node,...]}
-for row in rows:
-    osmid_way = row[0]
-    way2nodes[str(osmid_way)] = row[1]
-conn.commit()
-cur.close()
-conn.close()
-
-
+# conn = psycopg2.connect(database="porto", user="osmuser", password="pass", host="172.19.7.241", port="5432")
+# cur = conn.cursor()
+#
+# cur.execute("select id, nodes from ways;")
+# rows = cur.fetchall()        # all rows in table
+#
+# way2nodes = {}  # {osmid_way: [osmid_node,...]}
+# for row in rows:
+#     osmid_way = row[0]
+#     way2nodes[str(osmid_way)] = row[1]
+# conn.commit()
+# cur.close()
+# conn.close()
+#
+#
 walks = []
 f_500K_ways = open(r'500k.w2v.txt', 'r')
 f_500K_node2vec = open(r'500k.nodes.vector', 'w+')
-f_500K_nodes = open(r'500k.nodes.txt', 'w+')
+f_500K_nodes = open(r'500k.nodes.txt', 'r')
 # for line in f_500K_ways.readlines():
 #     output_nodes = []
 #     way_list = line.strip().split(" ")
@@ -42,14 +42,29 @@ f_500K_nodes = open(r'500k.nodes.txt', 'w+')
 #                 output_nodes.append(node)
 #                 index -= 1
 #     # f_500K_nodes.write(str(output_nodes) + '\n')
-#     walks.append(output_nodes)
-
+#     if len(output_nodes) > 1:
+#         walks.append(output_nodes)
+#
 # f_500K_nodes.write(json.dumps(walks))
 
-walks = json.loads(f_500K_nodes.readline())
+data = json.loads(f_500K_nodes.readline())
+print(len(data))
+count = 0
+for tra in data:
+    print(count)
+    count += 1
+    if len(tra) < 10:
+        continue
+    trajectory = []
+    for item in tra:
+        if type(item)==int:
+            item = str(item)
+        trajectory.append(item)
+    walks.append(trajectory)
 
 model = Word2Vec(walks, size=64, window=5, min_count=0, sg=1, hs=1, workers=4)
-model.wv.save_word2vec_format(f_500K_node2vec)
+model.save('mymodel')
+model.wv.save_word2vec_format("500k.nodes.vector")
 
 f_500K_ways.close()
 f_500K_node2vec.close()
