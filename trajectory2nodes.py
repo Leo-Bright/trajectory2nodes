4dfa76598f7f0e1aa7dd25ece180420765468e8c
Leo <xinming.lai@qq.com>
2018-12-09 23:35:02 +0800

    跑一下word2vec

--- trajectory2nodes.py
+++ trajectory2nodes.py
@@ -22,32 +22,34 @@ walks = []
 f_500K_ways = open(r'500k.w2v.txt', 'r')
 f_500K_node2vec = open(r'500k.nodes.vector', 'w+')
 f_500K_nodes = open(r'500k.nodes.txt', 'w+')
-for line in f_500K_ways.readlines():
-    output_nodes = []
-    way_list = line.strip().split(" ")
-    if len(way_list) < 1: continue
-    for way in way_list:
-        way = way.strip()
-        if len(way) < 1 : continue
-        osmid_way, heading = way.split("-")
-        if osmid_way not in way2nodes: continue
-        nodes = way2nodes[osmid_way]
-        if heading == "forward":
-            for node in nodes:
-                output_nodes.append(str(node))
-        else:
-            index = len(nodes) - 1
-            while index >= 0:
-                node = nodes[index]
-                output_nodes.append(node)
-                index -= 1
-    # f_500K_nodes.write(str(output_nodes) + '\n')
-    walks.append(output_nodes)
+# for line in f_500K_ways.readlines():
+#     output_nodes = []
+#     way_list = line.strip().split(" ")
+#     if len(way_list) < 1: continue
+#     for way in way_list:
+#         way = way.strip()
+#         if len(way) < 1 : continue
+#         osmid_way, heading = way.split("-")
+#         if osmid_way not in way2nodes: continue
+#         nodes = way2nodes[osmid_way]
+#         if heading == "forward":
+#             for node in nodes:
+#                 output_nodes.append(str(node))
+#         else:
+#             index = len(nodes) - 1
+#             while index >= 0:
+#                 node = nodes[index]
+#                 output_nodes.append(node)
+#                 index -= 1
+#     # f_500K_nodes.write(str(output_nodes) + '\n')
+#     walks.append(output_nodes)
 
-f_500K_nodes.write(json.dumps(walks))
+# f_500K_nodes.write(json.dumps(walks))
 
-# model = Word2Vec(walks, size=64, window=5, min_count=0, sg=1, hs=1, workers=4)
-# model.wv.save_word2vec_format(f_500K_node2vec)
+walks = json.loads(f_500K_nodes.readline())
+
+model = Word2Vec(walks, size=64, window=5, min_count=0, sg=1, hs=1, workers=4)
+model.wv.save_word2vec_format(f_500K_node2vec)
 
 f_500K_ways.close()
 f_500K_node2vec.close()
