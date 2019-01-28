79d2b3e954b335d698a9f5a2227c81f39f66f372
Leo Bright <buxingzhexiong@sina.com>
2019-01-28 17:28:57 +0800

    tokyo road_segments 统计

--- statistical_trajectory_time.py
+++ statistical_trajectory_time.py
@@ -1,28 +1,37 @@
 import json
+import time
+
+
+def time_str2time_stamp(s):
+    time_array = time.strptime(s, "%Y-%m-%d %H:%M:%S")
+    time_stamp = int(time.mktime(time_array))
+    return time_stamp
 
 
 def main(road_segment_file):
 
     trajectory_total_num = 0
-    points_total_num = 0
+    segs_total_num = 0
+    time_total_num = 0
     with open(road_segment_file, 'r') as f:
         for line in f:
             if line.startswith('"TRIP_ID"'):
                 continue
-            line_items = line.strip().split(',', 8)
-            cleaner_items = [ele.strip('"') for ele in line_items]
-            tra_points = json.loads(cleaner_items[8])
-            tra_size = len(tra_points)
-            if tra_size < 30:
-                continue
-            points_total_num += tra_size
+            # items = line.strip().split(',', 1)
+            road_segments = json.loads(line)
+            seg_size = len(road_segments)
+            segs_total_num += seg_size
             trajectory_total_num += 1
+            # time_total_num += time_str2time_stamp(tra_points[-1]['time'].split('+')[0]) - time_str2time_stamp(tra_points[0]['time'].split('+')[0])
+            time_total_num += road_segments[-1]['time'] - road_segments[0]['time']
 
     print('trajectory_total_num: ', trajectory_total_num)
-    print('points_total_num: ', points_total_num)
-    print('average trajectory points: ', points_total_num/trajectory_total_num)
+    print('segs_total_num: ', segs_total_num)
+    print('time_total_num: ', time_total_num)
+    print('average trajectory segs: ', segs_total_num/trajectory_total_num)
+    print('average segment times: ', time_total_num/segs_total_num)
 
 
 if __name__ == '__main__':
 
-    main('porto/dataset/train.csv', )
+    main('tokyo/sequence/tk_trajectory_transport_4_road_segment.sequence', )
