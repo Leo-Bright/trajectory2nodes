f861dd0d2b584433fabffd1928fd3b3967e822bf
Leo <xinming.lai@qq.com>
2019-01-29 21:09:41 +0800

    tokyo 统计 微调

--- statistical_trajectory_time.py
+++ statistical_trajectory_time.py
@@ -13,8 +13,8 @@ def main(road_segment_file, road_segment_file2, file3, file4):
     trajectory_total_num = 0
     point_total_num = 0
     time_total_num = 0
-    with open(road_segment_file, 'r') as f:
-        for line in f:
+    with open(road_segment_file, 'r') as f1:
+        for line in f1:
             if line.startswith('"TRIP_ID"'):
                 continue
             items = line.strip().split(',', 1)
@@ -23,7 +23,6 @@ def main(road_segment_file, road_segment_file2, file3, file4):
             point_total_num += points_size
             trajectory_total_num += 1
             time_total_num += time_str2time_stamp(tra_points[-1]['time'].split('+')[0]) - time_str2time_stamp(tra_points[0]['time'].split('+')[0])
-            # time_total_num += road_segments[-1]['time'] - road_segments[0]['time']
 
     with open(road_segment_file2, 'r') as f2:
         for line in f2:
@@ -35,39 +34,14 @@ def main(road_segment_file, road_segment_file2, file3, file4):
             point_total_num += points_size
             trajectory_total_num += 1
             time_total_num += time_str2time_stamp(tra_points[-1]['time'].split('+')[0]) - time_str2time_stamp(tra_points[0]['time'].split('+')[0])
-            # time_total_num += road_segments[-1]['time'] - road_segments[0]['time']
-
-    with open(file3, 'r') as f3:
-        for line in f3:
-            if line.startswith('"TRIP_ID"'):
-                continue
-            items = line.strip().split(',', 1)
-            tra_points = json.loads(items[1])
-            points_size = len(tra_points)
-            point_total_num += points_size
-            trajectory_total_num += 1
-            time_total_num += time_str2time_stamp(tra_points[-1]['time'].split('+')[0]) - time_str2time_stamp(tra_points[0]['time'].split('+')[0])
-            # time_total_num += road_segments[-1]['time'] - road_segments[0]['time']
-
-    with open(file4, 'r') as f4:
-        for line in f4:
-            if line.startswith('"TRIP_ID"'):
-                continue
-            items = line.strip().split(',', 1)
-            tra_points = json.loads(items[1])
-            points_size = len(tra_points)
-            point_total_num += points_size
-            trajectory_total_num += 1
-            time_total_num += time_str2time_stamp(tra_points[-1]['time'].split('+')[0]) - time_str2time_stamp(tra_points[0]['time'].split('+')[0])
-            # time_total_num += road_segments[-1]['time'] - road_segments[0]['time']
 
     print('trajectory_total_num: ', trajectory_total_num)
     print('point_total_num: ', point_total_num)
     print('time_total_num: ', time_total_num)
     print('average trajectory point: ', point_total_num/trajectory_total_num)
-    print('average segment times: ', time_total_num/point_total_num)
+    print('average point time: ', time_total_num/point_total_num)
 
 
 if __name__ == '__main__':
 
-    main('tokyo/request/transport_2.request', 'tokyo/request/transport_4.request', 'tokyo/request/transport_1.request', 'tokyo/request/transport_3.request',)
+    main('tokyo/request/transport_2.request', 'tokyo/request/transport_4.request')
