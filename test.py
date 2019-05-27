a217c92cbcae4f5bdce4f642ef7548159f652bc7
git <564312023@qq.com>
2019-05-27 09:37:33 +0800

    1. 微调，统计最长的trajectory

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 max_length = 0
+min_length = 999
 
 with open('porto/sequence/pt_trajectory_node_travel_time_50_70_1w.travel') as f:
     for line in f:
@@ -6,5 +7,8 @@ with open('porto/sequence/pt_trajectory_node_travel_time_50_70_1w.travel') as f:
         size = len(nodes_time)
         if size > max_length:
             max_length = size
+        if size < min_length:
+            min_length = size
 
 print("max_length: ", max_length)
+print("min_length: ", min_length)
