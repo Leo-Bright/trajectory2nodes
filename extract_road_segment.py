0a3829c4ef6e55f6cc51a9065b58aa48c915dfb4
Leo <xinming.lai@qq.com>
2019-07-29 23:46:21 +0800

    chore: remove some annotation

--- extract_road_segment.py
+++ extract_road_segment.py
@@ -1,9 +1,8 @@
 import psycopg2
-from gensim.models import Word2Vec
 import json
 
 # 数据库连接参数
-conn = psycopg2.connect(database="porto", user="osmuser", password="pass", host="localhost", port="5432")
+conn = psycopg2.connect(database="tokyo", user="osmuser", password="pass", host="localhost", port="5432")
 cur = conn.cursor()
 
 cur.execute("select gid, osm_id, class_id, source, target, reverse, priority from bfmap_ways;")
@@ -39,7 +38,7 @@ cur.close()
 conn.close()
 print(len(nodes_count.keys()))
 
-with open(r'all_road_segments_dict.porto', 'w+') as f:
+with open(r'all_road_segments_dict.tokyo', 'w+') as f:
     f.write(json.dumps(all_road_segment_dict))
 
 with open(r'node_types.porto', 'w+') as node_type:
@@ -99,55 +98,3 @@ with open(r'porto_nodes2segment.json', 'w+') as nodes2segment_file:
     nodes2segment_file.write(json.dumps(nodes2segment))
 
 
-
-# walks = []
-# f_500K_ways = open(r'500k.w2v.txt', 'r')
-# f_500K_node2vec = open(r'500k.nodes.vector', 'w+')
-# f_500K_nodes = open(r'500k.nodes.txt', 'r')
-# for line in f_500K_ways.readlines():
-#     output_nodes = []
-#     way_list = line.strip().split(" ")
-#     if len(way_list) < 1: continue
-#     for way in way_list:
-#         way = way.strip()
-#         if len(way) < 1 : continue
-#         osmid_way, heading = way.split("-")
-#         if osmid_way not in way2nodes: continue
-#         nodes = way2nodes[osmid_way]
-#         if heading == "forward":
-#             for node in nodes:
-#                 output_nodes.append(str(node))
-#         else:
-#             index = len(nodes) - 1
-#             while index >= 0:
-#                 node = nodes[index]
-#                 output_nodes.append(node)
-#                 index -= 1
-#     # f_500K_nodes.write(str(output_nodes) + '\n')
-#     if len(output_nodes) > 1:
-#         walks.append(output_nodes)
-#
-# f_500K_nodes.write(json.dumps(walks))
-#
-# data = json.loads(f_500K_nodes.readline())
-# print(len(data))
-# count = 0
-# for tra in data:
-#     print(count)
-#     count += 1
-#     if len(tra) < 10:
-#         continue
-#     trajectory = []
-#     for item in tra:
-#         if type(item)==int:
-#             item = str(item)
-#         trajectory.append(item)
-#     walks.append(trajectory)
-#
-# model = Word2Vec(walks, size=64, window=5, min_count=0, sg=1, hs=1, workers=4)
-# model.save('mymodel')
-# model.wv.save_word2vec_format("500k.nodes.vector")
-
-# f_500K_ways.close()
-# f_500K_node2vec.close()
-# f_500K_nodes.close()
