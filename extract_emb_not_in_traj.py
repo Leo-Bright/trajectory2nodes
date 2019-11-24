efe82e33626614be2c26019c912420c55b217097
Leo Bright <buxingzhexiong@sina.com>
2019-11-25 00:15:02 +0800

    Feature: add extract_emb_not_in_traj func init.

---
+++ extract_emb_not_in_traj.py
@@ -0,0 +1,60 @@
+import json
+
+
+def main(shortest_path, trajectory, shortest_emb):
+
+    with open(output, 'w+') as f:
+        for _node_seq in get_node_seqs(input_sequence):
+            node_seq = []
+            node_seq.append(_node_seq[0])
+            pointer = 1
+            for node in _node_seq[1:]:
+                if _node_seq[pointer - 1] != node:
+                    node_seq.append(node)
+                pointer += 1
+            num = len(node_seq)//100
+            for i in range(100):
+                start = num * i
+                end = num * (i + 1)
+                seq = node_seq[start:end]
+                f.write('%s\n' % ' 0 '.join(map(str, seq)))
+
+
+def get_node_seqs(input_seq):
+    with open(input_seq) as input_seq_file:
+        for seq_line in input_seq_file:
+            road_segment_seq = json.loads(seq_line)
+            if len(road_segment_seq) < 10:
+                continue
+            else:
+                _node_seq = segments2node_seq(road_segment_seq)
+                yield _node_seq
+
+
+def segments2node_seq(road_segment_seq):
+    nodes = []
+    is_head = True
+    for road_segment in road_segment_seq:
+        if is_head:
+            node = road_segment["target"] if road_segment["heading"] == "forward" else road_segment["source"]
+            nodes.append(node)
+            is_head = False
+        else:
+            if road_segment["heading"] == "forward":
+                _source = road_segment["source"]
+                _target = road_segment["target"]
+            else:
+                _source = road_segment["target"]
+                _target = road_segment["source"]
+            nodes.append(_source)
+            nodes.append(_target)
+    return nodes
+
+
+if __name__ == '__main__':
+
+    main(shortest_path='sanfrancisco/sequence/sf_trajectory_road_segment.sequence',
+         trajectory='sanfrancisco/sequence/sf_trajectory_sequence.node',
+         shortest_emb='',
+         output_node='',
+         output_emb='')
